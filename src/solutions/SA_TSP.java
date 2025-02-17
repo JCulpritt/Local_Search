ba7de079cdd4a9611c6f64ffe8a061e6d9c8fb82
ggrim22 <gtgrim22@gmail.com>
2025-02-17 10:58:29 -0500

    Add test cases

--- src/solutions/SA_TSP.java
+++ src/solutions/SA_TSP.java
@@ -5,8 +5,8 @@ import problems.TSP;
 
 public class SA_TSP extends SimulatedAnnealing<int[]> {
     private final static long INIT_TIME = 1;
-    private final static double INIT_TEMP = 1e80;
-    private final static long MAX_TIME = 300_000_000;
+    private final static double INIT_TEMP = 1e13;
+    private final static long MAX_TIME = 100_000_000;
 
     public SA_TSP(TSP problem) {
         super(INIT_TIME, INIT_TEMP, problem);
